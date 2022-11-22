48bffb6f32533e67723516acb0a9a089804e72ad
Roy Anderson <royanderson.k@gmail.com>
2022-11-22 09:11:50 -0600

    style SpeciesPage component

--- client/src/components/SpeciesPage.js
+++ client/src/components/SpeciesPage.js
@@ -7,33 +7,37 @@ import LoadingIcon from "./LoadingIcon"
 const SpeciesPage = () => {
   const location = useLocation()
   const taxon = location.state
-
-  console.log(taxon)
   
   const { data, isFetching } = useWikiSummary(taxon.wikipedia_url)
 
   return (
-    <main className="container">
+    <main className="page-container">
       <div className="page-left">
-        <div className="page-text">
-          <div>
+        <div>
+          <div className="page-title">
             <div className="page-names">
-              <div>{taxon.preferred_common_name}</div>
-              <div>{taxon.name}</div>
+              <div className="page-common-name">{taxon.preferred_common_name}</div>
+              <div className="page-sci-name">{taxon.name}</div>
             </div>
-            <button>Add to set</button>
-          </div>
-          <div className="page-wiki">
-            {isFetching
-              ? <LoadingIcon />
-              : data
-                ? data
-                : <>No Wikipedia information</>
-            }
+            <button className="page-button">
+              <div className="button-text">Add to set</div>
+              <div className="button-plus">+</div>
+            </button>
           </div>
         </div>
-        <img src={getDefaultPhotoUrl(taxon)} alt="species" />
+        <div className="page-wiki">
+          {isFetching
+            ? <LoadingIcon />
+            : data
+              ? <>
+                <div className="page-wiki-info">{data}</div>
+                <div className="page-wiki-cite">Information from <a href={taxon.wikipedia_url}>Wikipedia</a></div>
+                </>
+              : <>No Wikipedia information</>
+          }
+        </div>
       </div>
+      <img className="page-img" src={getDefaultPhotoUrl(taxon)} alt="species" />
     </main>
   )
 }
