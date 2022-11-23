ff68b93bf7f4c6200abb71377f08425c48ea0794
Roy Anderson <royanderson.k@gmail.com>
2022-11-23 13:01:25 -0600

    style login page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,15 +1,42 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 const LoginPage = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(true)
+
   return (
-    <div>
-      <form>
-        <div>
-          <input type="text" />
+    <div className="login-container">
+      <div className="login-left">
+        <div className='login-left-text'>
+          <p>Every species in the world.</p>
+          <p>All in one place.</p>
         </div>
-        <div>
-          <input type="text" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      </div>
+      <div className="login-right">
+        <form>
+          <div className="login-fields">
+            {error &&
+              <div className="login-fields-error">
+                <p>
+                  Incorrect username or password.
+                </p>
+                <p>
+                  Don't have an account? <Link className='login-link' to='/signup'>Sign up</Link>.
+                </p>
+              </div>
+            }
+            <div className="login-fields-username">
+              <input type="text" placeholder='Username' />
+            </div>
+            <div className="login-fields-password">
+              <input type="password" placeholder='Password' />
+            </div>
+          </div>
+          <button className="login-submit" type="submit">Log in</button>
+        </form>
+      </div>
     </div>
   )
 }
