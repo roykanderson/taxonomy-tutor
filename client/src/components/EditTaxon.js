99d57dbe67bc36a711ae4a2bdeb3ceff7235ecb1
Roy Anderson <royanderson.k@gmail.com>
2023-03-16 14:35:19 -0700

    adapted CreateTaxon component as EditTaxon component

---
+++ client/src/components/EditTaxon.js
@@ -0,0 +1,89 @@
+import { useState } from "react"
+
+import taxaService from "../services/taxaService"
+import observationsService from "../services/observations"
+
+const EditTaxon = ({ taxa, setTaxa, index }) => {
+
+  const [input, setInput] = useState(taxa[index].preferred_common_name)
+  const [focused, setFocused] = useState(false)
+  const [suggestions, setSuggestions] = useState(null)
+
+  const handleChange = async ({ target }) => {
+    setInput(target.value)
+    if (target.value) {
+      const results = await taxaService.fetchTaxaSuggestions(target.value)
+      setSuggestions(results.filter(result => result.rank === 'species'))
+    } else {
+      setSuggestions(null)
+    }
+  }
+
+  const onBlur = () => {
+    setFocused(false)
+    setSuggestions(null)
+    setInput(taxa[index].preferred_common_name)
+  }
+  
+  const handleSuggestionClick = async ({ target }) => {
+    const id = target.getAttribute('data-id')
+    const newTaxon = await observationsService.fetchTaxaById(id)
+    const newTaxa = taxa.slice()
+    newTaxa[index] = newTaxon
+
+    setTaxa(newTaxa)
+    setInput(newTaxon.preferred_common_name)
+  }
+
+  const handleRemove = () => {
+    const newTaxa = taxa.slice()
+    newTaxa.splice(index, 1)
+    setTaxa(newTaxa)
+  }
+
+  return (
+    <>
+      <div className={focused ? 'create-taxon active' : 'create-taxon'}>
+        <img src={taxa[index].default_photo.url} alt={taxa[index].preferred_common_name} />
+        <div className="create-taxon-left">
+          <div> 
+            {index + 1}
+          </div>
+          <input
+            className="create-taxon-input"
+            type="text"
+            value={input}
+            onChange={handleChange}
+            onFocus={() => setFocused(true)}
+            onBlur={onBlur}
+          />
+          {!focused && 
+            <div className="create-sci">
+              {taxa[index].name}
+            </div>
+          }
+        </div>
+        <button className="create-taxon-button" onClick={handleRemove}>
+          Remove
+        </button>
+      </div>
+      <ul className='create-suggestions taxon'>
+        {suggestions && suggestions.map(suggestion =>
+          <li key={suggestion.id} className='create-suggestion' onMouseDown={handleSuggestionClick} data-id={suggestion.id}>
+            {suggestion.default_photo &&
+              <img src={suggestion.default_photo.url} alt={suggestion.preferred_common_name} data-id={suggestion.id} />
+            }
+            <div className="create-suggestion-common" data-id={suggestion.id}>
+              {suggestion.preferred_common_name}
+            </div>
+            <div className="create-suggestion-sci" data-id={suggestion.id}>
+              {suggestion.name}
+            </div>
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default EditTaxon
--- client/src/components/StudyEdit.js
+++ client/src/components/StudyEdit.js
@@ -1,7 +1,49 @@
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
+
+import CreateAddButton from "./CreateAddButton"
+import EditTaxon from "./EditTaxon"
+import CreateTitle from "./CreateTitle"
+
+import observationsService from "../services/observations"
+
 const StudyEdit = () => {
+  const location = useLocation()
+  const set = location.state
+
+  const [title, setTitle] = useState(set.name)
+  const [taxa, setTaxa] = useState([])
+
+  console.log('TAXA', taxa)
+
+  useEffect(() => {
+    const fetchTaxaData = async () => {
+      const data = set.taxonIds.map(async (id) => {
+        return await observationsService.fetchTaxaById(id)
+      })
+      setTaxa(await Promise.all(data))
+    }
+
+    fetchTaxaData()
+  }, [set.taxonIds])
+
+  const handleUpdate = () => {
+    return null
+  }
+
   return (
-    <div className="study-edit">
-      StudyEdit
+    <div className="create-container">
+      <CreateTitle title={title} setTitle={setTitle} />
+      {taxa.map((taxon, index) =>
+        <EditTaxon key={taxon.id} taxa={taxa} setTaxa={setTaxa} index={index} />
+      )}
+      <CreateAddButton
+        taxa={taxa}
+        setTaxa={setTaxa}
+      />
+      <button className="create-submit" onClick={handleUpdate}>
+        Update
+      </button>
     </div>
   )
 }
