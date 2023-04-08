8270c25e2844369bdb64d651722f238ce0790b82
Roy Anderson <royanderson.k@gmail.com>
2023-04-07 20:38:19 -0700

    automatically scroll to top of window whenever page changes

--- client/src/pages/Search/SearchPage.js
+++ client/src/pages/Search/SearchPage.js
@@ -6,13 +6,23 @@ import { ReactComponent as NextArrowGreen } from '../../assets/next-arrow-green.
 import styles from './styles/SearchPage.module.css'
 
 const SearchPage = ({ data, page, setPage }) => {
+  const handleBackClick = () => {
+    setPage(page - 1)
+    window.scrollTo(0, 0)
+  }
+
+  const handleForwardClick = () => {
+    setPage(page + 1)
+    window.scrollTo(0, 0)
+  }
+
   return (
     <>
       {data &&
         <div className={styles.SearchPage}>
           <button
             className={page === 1 ? `${styles.SearchPage__button}` : `${styles.SearchPage__button} ${styles['SearchPage__button--active']}`}
-            onClick={() => setPage(page - 1)}
+            onClick={handleBackClick}
             disabled={page === 1}
           >
             {page === 1 ? <BackArrowGrey /> : <BackArrowGreen />}
@@ -20,7 +30,7 @@ const SearchPage = ({ data, page, setPage }) => {
           Page {page} of {Math.ceil(data.total_results / data.per_page)}
           <button
             className={page === Math.ceil(data.total_results / data.per_page) ? `${styles.SearchPage__button}` : `${styles.SearchPage__button} ${styles['SearchPage__button--active']}`}
-            onClick={() => setPage(page + 1)}
+            onClick={handleForwardClick}
             disabled={page === Math.ceil(data.total_results / data.per_page)}
           >
             {page === Math.ceil(data.total_results / data.per_page) ? <NextArrowGrey /> : <NextArrowGreen />}
