c2d481c06dc2aec499bd8102d955ea0c9bd559c2
Roy Anderson <royanderson.k@gmail.com>
2023-04-08 20:30:49 -0700

    migrate styles to css module

--- client/src/pages/Profile/ProfileCreate.js
+++ client/src/pages/Profile/ProfileCreate.js
@@ -8,6 +8,8 @@ import CreateAdd from "./CreateAdd"
 import CreateTaxon from "./CreateTaxon"
 import CreateTitle from "./CreateTitle"
 
+import styles from './ProfileCreate.module.css'
+
 const ProfileCreate = () => {
   const navigate = useNavigate()
   const queryClient = useQueryClient()
@@ -23,7 +25,7 @@ const ProfileCreate = () => {
   }
 
   return (
-    <div className="create-container">
+    <div className={styles.ProfileCreate}>
       <CreateTitle title={title} setTitle={setTitle} />
       {taxa.map((taxon, index) =>
         <CreateTaxon key={taxon.id} taxa={taxa} setTaxa={setTaxa} index={index} />
@@ -32,7 +34,7 @@ const ProfileCreate = () => {
         taxa={taxa}
         setTaxa={setTaxa}
       />
-      <button className={title ? 'create-submit' : 'create-submit inactive'} onClick={handleCreate} disabled={!title}>
+      <button className={title ? `${styles.ProfileCreate__submit}` : `${styles.ProfileCreate__submit} ${styles['ProfileCreate__submit--inactive']}`} onClick={handleCreate} disabled={!title}>
         Create
       </button>
     </div>
