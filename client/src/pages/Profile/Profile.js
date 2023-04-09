ab4e3735b1357ac3ce8bb272175435585e10747d
Roy Anderson <royanderson.k@gmail.com>
2023-04-08 20:21:10 -0700

    migrate styles to css module

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,29 +2,31 @@ import { useContext } from 'react'
 import { UserContext } from '../../context/UserContext'
 import { Link, Outlet, useLocation } from 'react-router-dom'
 
+import styles from './Profile.module.css'
+
 const Profile = () => {
   const { user } = useContext(UserContext)
 
   const location = useLocation()
 
   return (
-    <main className="profile-container">
-      <div className="profile-banner">
-        <div>
+    <main className={styles.Profile}>
+      <div className={styles.Profile__banner}>
+        <div className={styles.Profile__icon}>
           {user.username.charAt(0).toUpperCase()}
         </div>
-        <div>
+        <div className={styles.Profile__username}>
           {user.username}
         </div>
       </div>
-      <div className='profile-options'>
-        <button className={location.pathname === '/profile' ? 'active' : ''}>
-          <Link to=''>
+      <div className={styles.Profile__options}>
+        <button className={location.pathname === '/profile' ? `${styles.Profile__button} ${styles['Profile__button--active']}` : `${styles.Profile__button}`}>
+          <Link className={styles.Profile__link} to=''>
             My species sets
           </Link>
         </button>
-        <button className={location.pathname === '/profile/create' ? 'active' : ''}>
-          <Link to='create'>
+        <button className={location.pathname === '/profile/create' ? `${styles.Profile__button} ${styles['Profile__button--active']}` : `${styles.Profile__button}`}>
+          <Link className={styles.Profile__link} to='create'>
             Create new set
           </Link>
         </button>
