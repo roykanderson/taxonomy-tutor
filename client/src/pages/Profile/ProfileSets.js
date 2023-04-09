97cb5697fc4276099c7a6d2bf9ad78c99f4f7311
Roy Anderson <royanderson.k@gmail.com>
2023-04-09 09:54:58 -0700

    migrate styles to css module

--- client/src/pages/Profile/ProfileSets.js
+++ client/src/pages/Profile/ProfileSets.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import useSets from '../../hooks/useSets'
 import LoadingIcon from "../../components/LoadingIcon"
 
+import styles from './ProfileSets.module.css'
+
 const ProfileSets = () => {
   const { data, isFetching, isError, error } = useSets()
 
@@ -14,7 +16,7 @@ const ProfileSets = () => {
 
   else if (isError) {
     return (
-      <div className='error'>
+      <div>
         {error}
       </div>
     )
@@ -22,26 +24,26 @@ const ProfileSets = () => {
 
   else if (data.length === 0) {
     return (
-      <div className='sets-nosets'>
+      <div className={styles.ProfileSets__noSets}>
         It looks like you haven't created any study sets yet. Now's the perfect time to start!
       </div>
     )
   }
 
   return (
-    <div className="sets-container">
+    <div className={styles.ProfileSets}>
       {
         data
           .sort((set1, set2) => new Date(set2.dateLastUpdated) - new Date(set1.dateLastUpdated)) // Sort sets by most recently updated
           .map(set => 
-            <Link key={set.id} className="sets-set" to={`/profile/${set.id}`}>
-              <p>
+            <Link key={set.id} className={styles.ProfileSets__set} to={`/profile/${set.id}`}>
+              <p className={styles.ProfileSets__name}>
                 {set.name}
               </p>
-              <p>
+              <p className={styles.ProfileSets__date}>
                 {new Date(set.dateLastUpdated).toLocaleDateString('en-us', { year: 'numeric', month: 'long', day: 'numeric' })}
               </p>
-              <p>
+              <p className={styles.ProfileSets__numSpecies}>
                 {set.numberOfTaxa} species
               </p>
             </Link>
