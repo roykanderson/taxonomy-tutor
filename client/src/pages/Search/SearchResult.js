256016bc74507c2b5910bce443b9110950d4dd8c
Roy Anderson <royanderson.k@gmail.com>
2023-04-10 15:07:05 -0700

    add responsive styles to Search results page

--- client/src/pages/Search/SearchResult.js
+++ client/src/pages/Search/SearchResult.js
@@ -9,9 +9,12 @@ import styles from './SearchResult.module.css'
 const SearchResult = ({ taxon }) => {
   return (
     <Link to={`/search/${taxon.id}`} state={taxon} className={styles.SearchResult}>
-      <img className={styles.SearchResult__image} src={getDefaultPhotoUrl(taxon)} alt="" />
+      {taxon.default_photo
+        ? <img className={styles.SearchResult__image} src={getDefaultPhotoUrl(taxon)} alt="" />
+        : <div className={styles.SearchResult__noImage}>No image available.</div>
+      }
       <div className={styles.SearchResult__namesContainer}>
-        <p className={styles.SearchResult__commonName}>{taxon.preferred_common_name}</p>
+        <p className={styles.SearchResult__commonName}>{taxon.preferred_common_name ? taxon.preferred_common_name : taxon.name}</p>
         <p className={styles.SearchResult__sciName}>{taxon.name}</p>
       </div>
     </Link>
