395ef1c39b7c34a50ffa4d4d36a12458df8eed85
Roy Anderson <royanderson.k@gmail.com>
2023-04-13 11:34:42 -0700

    add option to decrease size by passing size prop

--- client/src/components/LoadingIcon.js
+++ client/src/components/LoadingIcon.js
@@ -1,6 +1,14 @@
 import styles from './LoadingIcon.module.css'
 
-const LoadingIcon = () => {
+const LoadingIcon = ({ size }) => {
+  if (size === 'small') {
+    return (
+      <div className={styles.LoadingIcon}>
+        <div className={`${styles.LoadingIcon__spinner} ${styles['LoadingIcon__spinner--small']}`}></div>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.LoadingIcon}>
       <div className={styles.LoadingIcon__spinner}></div>
