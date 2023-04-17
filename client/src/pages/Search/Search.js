b2f624f59a00cb7e9ec625ee08bbda790de4d2d6
Roy Anderson <royanderson.k@gmail.com>
2023-04-17 13:50:56 -0700

    update search result grid styles

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -31,7 +31,7 @@ const Search = () => {
       </section>
       {isFetching
         ? <LoadingIcon />
-        : <section className={data.results.length === 1 ? `${styles.Search__results} ${styles['Search__results--single']}` : `${styles.Search__results}`}>
+        : <section className={data.results.length < 5 ? `${styles.Search__results} ${styles['Search__results--singleRow']}` : `${styles.Search__results}`}>
           {data.results.map(taxon => 
               <SearchResult key={taxon.id} taxon={taxon} />     
           )}
