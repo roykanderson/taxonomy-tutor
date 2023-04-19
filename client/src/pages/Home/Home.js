ccbda97e8cfbaadb021170e845fa5d868a0cf200
Roy Anderson <royanderson.k@gmail.com>
2023-04-18 23:07:34 -0700

    add login button to homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -36,10 +36,15 @@ const Home = () => {
         </p>
       </section>
       {!user &&
-        <section className={styles.Home__callToAction}>
+        <section className={styles.Home__buttonsContainer}>
           <Link className={styles.Home__link} to='/signup'>
-            <button className={styles.Home__button}>
-              Create an account to get started!
+            <button className={styles.Home__signupButton}>
+              Create an account to get started
+            </button>
+          </Link>
+          <Link className={styles.Home__link} to='/login'>
+            <button className={styles.Home__loginButton}>
+              Log in
             </button>
           </Link>
         </section>
